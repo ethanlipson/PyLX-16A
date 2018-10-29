d62d5a25550e236fe2f141d583c5ca58c88357cd
swimingduck <ethan.lipson@gmail.com>
2018-10-28 20:37:57 -0400

    Added comments

--- helloWorld.py
+++ helloWorld.py
@@ -3,6 +3,8 @@ from math import sin, cos
 
 # This is the port that the controller board is connected to
 # This will be different for different computers
+# On Windows, try the ports COM1, COM2, COM3, etc...
+# On Raspbian, try each port in /dev/
 LX16A.initialize('/dev/ttyUSB0')
 
 # There should two servos connected, with IDs 1 and 2
